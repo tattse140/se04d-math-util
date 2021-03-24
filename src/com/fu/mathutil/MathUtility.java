2ccdab485a850178e696dbc29a40bad176b2ea8f
tattse140 <ttrungta2031@gmail.com>
2021-03-24 08:29:19 +0700

    GREEN 4EVER

--- src/com/fu/mathutil/MathUtility.java
+++ src/com/fu/mathutil/MathUtility.java
@@ -29,7 +29,7 @@ public class MathUtility {
             if( n==0 || n ==1)
                 return 1; 
             
-            long result = 0;// co tinh de value nay
+            long result = 1;// co tinh de value nay
              
             for(int i =2; i<=n; i++)
                  result *=i;
